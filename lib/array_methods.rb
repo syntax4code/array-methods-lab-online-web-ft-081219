eb95173d9bc712defee24fb76d14eb61aae0131e
Warren Loy Harper II <warrenharper2@att.net>
2019-08-14 22:25:16 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,32 @@
 def using_include(array, element)
+  array = ["happy", "sad", "mad", "jelous"]
+  array.include?(sad)
+end
 
 end
 
 def using_sort(array)
-
+  array = ["happy", "sad", "mad", "jelous"]
+array.sort
 end
 
 def using_reverse(array)
-
+array = ["happy", "sad", "mad", "jelous"]
+array.reverse
 end
 
 def using_first(array)
-
+array = ["happy", "sad", "mad", "jelous"]
+array.first
 end
 
 def using_last(array)
+array = ["happy", "sad", "mad", "jelous"]  
+array.last
 
 end
 
 def using_size(array)
-
+array = ["happy", "sad", "mad", "jelous"]  
+array.size 
 end
